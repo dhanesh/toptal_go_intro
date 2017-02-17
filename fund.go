6bce62eab374c90dd2a9f46716fde096b82c6e29
Dhanesh Purohit <dpurohit@cimpress.com>
2017-02-17 18:56:49 +0530

    A Simple Fund & Testing with benchmarks

---
+++ fund.go
@@ -0,0 +1,26 @@
+package funding
+
+//Fund Structure to store balance
+type Fund struct {
+	// balance is unexported (private), because it's lowercase
+	balance int
+}
+
+//NewFund A regular function returning a pointer to a fund
+func NewFund(initialBalance int) *Fund {
+	// We can return a pointer to a new struct without worrying about
+	// whether it's on the stack or heap: Go figures that out for us.
+	return &Fund{
+		balance: initialBalance,
+	}
+}
+
+//Balance Methods start with a *receiver*, in this case a Fund pointer
+func (f *Fund) Balance() int {
+	return f.balance
+}
+
+//Withdraw Withdraw amount from balance
+func (f *Fund) Withdraw(amount int) {
+	f.balance -= amount
+}
